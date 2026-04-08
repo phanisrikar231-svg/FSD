fddccc1d8401c6c37102e10e989d63039e8a32b1
phanisrikar231-svg <phanisrikar231@gmail.com>
2026-04-08 10:42:06 +0530

    Add files via upload

---
+++ maven-archetype-webapp(TASK 9)/src/main/java/com/employee/WebAppInitializer.java
@@ -0,0 +1,22 @@
+package com.employee;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebAppInitializer extends 
+    AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] { AppConfig.class };
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] { "/" };
+    }
+}
